98bbb0eb90816272351bff43ed6ebe96cfc34da8
Yael Vaizgan <yaelv@armentavet.com>
2022-02-01 18:04:39 +0200

    Add project files.

---
+++ RemoteControl/RemoteControl/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RemoteControl
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_CMT_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CMTPage());
+        }
+
+        private async void Image_Support_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SupportPage());
+        }
+
+        private void Image_Home_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
